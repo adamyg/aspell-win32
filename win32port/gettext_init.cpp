b5dd6d6426cb3d4ab2932877663ab09fed12f711
adamyg <adamyg@gmail.com>
2022-01-25 00:47:19 +0800

    ENABLE_NLS

--- win32port/gettext_init.cpp
+++ win32port/gettext_init.cpp
@@ -4,29 +4,28 @@
 
 #include "gettext.h"
 
-#if ENABLE_NLS
+#if !defined(ENABLE_NLS)
+#error ENABLE_NLS not enabled ...
+#endif
+#pragma message("*** ENABLE_NLS enabled ...")
 
 #include "lock.hpp"
 #include "dirs.h" // aspell_LOCALE_DIR()
 
 static acommon::Mutex lock;
-
 static bool did_init = false;
 
-extern "C" void aspell_gettext_init()
+extern "C" void
+aspell_gettext_init()
 {
-  {
-    acommon::Lock l(&lock);
-    if (did_init) return;
-    did_init = true;
-  }
-  bindtextdomain("aspell", aspell_LOCALE_DIR());
-}
-
-#else
-
-extern "C" void aspell_gettext_init()
-{
-}
+#if defined(ENABLE_NLS)
+    {
+        acommon::Lock l(&lock);
+        if (did_init) return;
+        did_init = true;
+    }
+    bindtextdomain("aspell", aspell_LOCALE_DIR());
 
 #endif
+}
+
